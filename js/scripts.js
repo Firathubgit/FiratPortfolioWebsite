a98a3bc3ddf8f525f310ba63bec2e06a4cfb03df
Firathubgit <Firat05_@hotmail.com>
2024-07-24 22:35:42 +0200

    gr

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,15 @@
-// master GSAP and ScrollTrigger
-// https://www.creativeCodingClub.com
-// Unlock over 250 video lessons
+gsap.registerPlugin(ScrollTrigger);
 
+let sections = gsap.utils.toArray(".panel");
 
-const races = document.querySelector(".races");
-console.log(races.offsetWidth)
-
-function getScrollAmount() {
-	let racesWidth = races.scrollWidth;
-	return -(racesWidth - window.innerWidth);
-}
-
-const tween = gsap.to(races, {
-	x: getScrollAmount,
-	duration: 3,
-	ease: "none",
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".container").offsetWidth
+  }
 });
-
-
-ScrollTrigger.create({
-	trigger:".racesWrapper",
-	start:"top 20%",
-	end: () => `+=${getScrollAmount() * -1}`,
-	pin:true,
-	animation:tween,
-	scrub:1,
-	invalidateOnRefresh:true,
-	markers:true
-})
-
